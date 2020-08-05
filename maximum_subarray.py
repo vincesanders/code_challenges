9f642c97cd5911f5794d3b44de0ce1f0e4d3a4a7
Vincent Sanders <vincesanders@hotmail.com>
2020-08-05 05:59:59 -0500

    Added comments to max subarray question and did some light refactoring.

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -2,19 +2,25 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_sub_arr_num = nums[0]
-        current_total = nums[0]
+        # Initialize values with value at first index of nums
+        # current_best tracks max found so far
+        current_best = nums[0]
+        # best_this_index tracks the best at the current index
+        best_this_index = nums[0]
         
         for i in range(1, len(nums)):
-            if current_total + nums[i] > nums[i]:
-                current_total = current_total + nums[i]
+            # Determine whether current number is better than the 
+            # current number + the max of previous index
+            if best_this_index + nums[i] > nums[i]:
+                best_this_index = best_this_index + nums[i]
+            # If current number is larger than adding the current 
+            # number to previous index max, start a new subarray
             else:
-                current_total = nums[i]
+                best_this_index = nums[i]
 
-            if current_total < 0:
-                current_total = nums[i]
-
-            if current_total > max_sub_arr_num:
-                max_sub_arr_num = current_total
+            # Is the max found at this index better than 
+            # any other max we've found so far?
+            if best_this_index > current_best:
+                current_best = best_this_index
         
-        return max_sub_arr_num
+        return current_best
